c59dd4882aa9aa544082844ca11068862fd66554
Harryk89 <s.artem1989@mail.ru>
2018-06-14 23:17:10 +0300

    Add files via upload

--- main2.js
+++ main2.js
@@ -1,58 +1,44 @@
 window.onload = function() {
-	var main = new Main();
-}
 
-function Main() { 	
-	var self = this;
-	this.webGL=true;
-	this.width = document.documentElement.clientWidth;
-	this.height = document.documentElement.clientHeight;
-	this.app = new PIXI.Application(this.width, this.height);	
-	document.body.appendChild(this.app.view);
-	this.app.renderer.autoResize = true;
+	let webGL=true;
+	let width = document.documentElement.clientWidth;
+	let height = document.documentElement.clientHeight;
+	let app = new PIXI.Application(width, height);	
+	document.body.appendChild(app.view);
+	app.renderer.autoResize = true;
 
-	this.container = new PIXI.Container();
-	this.app.stage.addChild(this.container);
-	this.app.renderer.resize(this.width, this.height);
+	app.renderer.resize(width, height);
 
-	this.runDown = [
-		'sniper_move_cam_180/sniper_move_cam_180_00000.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00001.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00002.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00003.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00004.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00005.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00006.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00007.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00008.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00009.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00010.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00011.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00012.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00013.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00014.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00015.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00016.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00017.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00018.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00019.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00020.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00021.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00022.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00023.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00024.png',
-		'sniper_move_cam_180/sniper_move_cam_180_00025.png'
-	]
+	let frames;
+	let frames2;
+	let frames3;
+	let anim;
 
-	this.textureArrayDown = [];
-
-	for (var i=0; i < 20; i++)
-	{
-	     this.textureRunDown = PIXI.Texture.fromImage(this.runDown[i]);
-	     this.textureArrayDown.push(this.textureRunDown);
-	};
-
-	this.runRight = [
+	PIXI.loader
+    .add([
+		'sniper_death/sniper_death_00000.png',
+		'sniper_death/sniper_death_00001.png',
+		'sniper_death/sniper_death_00002.png',
+		'sniper_death/sniper_death_00003.png',
+		'sniper_death/sniper_death_00004.png',
+		'sniper_death/sniper_death_00005.png',
+		'sniper_death/sniper_death_00006.png',
+		'sniper_death/sniper_death_00007.png',
+		'sniper_death/sniper_death_00008.png',
+		'sniper_death/sniper_death_00009.png',
+		'sniper_death/sniper_death_00010.png',
+		'sniper_death/sniper_death_00011.png',
+		'sniper_death/sniper_death_00012.png',
+		'sniper_death/sniper_death_00013.png',
+		'sniper_death/sniper_death_00014.png',
+		'sniper_death/sniper_death_00015.png',
+		'sniper_death/sniper_death_00016.png',
+		'sniper_death/sniper_death_00017.png',
+		'sniper_death/sniper_death_00018.png',
+		'sniper_death/sniper_death_00019.png',
+		'sniper_death/sniper_death_00020.png'
+	])
+	.add([
 		'sniper_move_cam_270/sniper_move_cam_270_00000.png',
 		'sniper_move_cam_270/sniper_move_cam_270_00001.png',
 		'sniper_move_cam_270/sniper_move_cam_270_00002.png',
@@ -79,25 +65,66 @@ function Main() {
 		'sniper_move_cam_270/sniper_move_cam_270_00023.png',
 		'sniper_move_cam_270/sniper_move_cam_270_00024.png',
 		'sniper_move_cam_270/sniper_move_cam_270_00025.png'
-	];
+	])
+	.add([
+		'sniper_move_cam_180/sniper_move_cam_180_00000.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00001.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00002.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00003.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00004.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00005.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00006.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00007.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00008.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00009.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00010.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00011.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00012.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00013.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00014.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00015.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00016.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00017.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00018.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00019.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00020.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00021.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00022.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00023.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00024.png',
+		'sniper_move_cam_180/sniper_move_cam_180_00025.png'
+	])
+    .load(onAssetsLoaded);
 
-	this.textureArrayRight = [];
 
-	for (var i=0; i < 20; i++)
-	{
-	     this.textureRunRight = PIXI.Texture.fromImage(this.runRight[i]);
-	     this.textureArrayRight.push(this.textureRunRight);
-	};	
+    function onAssetsLoaded() {
 
-	this.warrior = new PIXI.extras.AnimatedSprite(this.textureArrayDown);
-	this.container.addChild(this.warrior);
-	this.warrior.animationSpeed = 0.4;
+	    frames = [];
+	    for (let i = 0; i < 25; i++) {
+	        let val = i < 10 ? '0' + i : i;
+	        frames.push(PIXI.Texture.fromFrame('sniper_move_cam_180/sniper_move_cam_180_000' + val + '.png'));
+	    }
 
-	this.warrior2 = new PIXI.extras.AnimatedSprite(this.textureArrayRight);
-	this.container.addChild(this.warrior2);
-	this.warrior2.animationSpeed = 0.4;
-	this.warrior2.visible = false;
-	var rightPressed,
+		frames2 = [];
+	    for (let i = 0; i < 25; i++) {
+	        let val = i < 10 ? '0' + i : i;
+	        frames2.push(PIXI.Texture.fromFrame('sniper_move_cam_270/sniper_move_cam_270_000' + val + '.png'));
+	    }
+
+	    frames3 = [];
+	    for (let i = 0; i < 20; i++) {
+	        let val = i < 10 ? '0' + i : i;
+	        frames3.push(PIXI.Texture.fromFrame('sniper_death/sniper_death_000' + val + '.png'));
+	    }
+
+	    anim = new PIXI.extras.AnimatedSprite(frames);
+	    anim.animationSpeed = 0.5;
+
+	    app.stage.addChild(anim);
+ 	}
+
+
+	let rightPressed,
 		downPressed,
 		upPressed,
 		leftPressed;
@@ -106,71 +133,68 @@ function Main() {
 
 	    if(e.keyCode === 39) {
 	        rightPressed = true;
-	        self.warrior.visible = false;
-	        self.warrior2.visible = true;
-	        self.warrior2.play();
+	        anim.textures = frames2;
+	        anim.play();
 	    }
 	    else if(e.keyCode === 40) {
 	        downPressed = true;
-	        self.warrior2.visible = false;
-	        self.warrior.visible = true;
-	        self.warrior.play();
+	        anim.textures = frames;
+	        anim.play();
 	    }
 	    else if(e.keyCode === 38) {
 	        upPressed = true;
-	        self.warrior2.visible = false;
-	        self.warrior.visible = true;
-	        self.warrior.play();
+	        anim.textures = frames;
+	        anim.play();
 	    }
 	    else if(e.keyCode === 37) {
 	        leftPressed = true;
-	        self.warrior.visible = false;
-	        self.warrior2.visible = true;
-	        self.warrior2.play();
+	        anim.textures = frames2;
+	        anim.play();
 	    }
 	}
 
 	document.onkeyup = function(e) {
 	    if(e.keyCode === 39 ) {
 	        rightPressed = false;
-	        self.warrior2.stop();	        
+	        anim.stop();        
 	    }
 	    else if(e.keyCode === 40) {
 	        downPressed = false;
-	        self.warrior.stop();
+	        anim.stop();
 	    }
 	    else if(e.keyCode === 38) {
 	        upPressed = false;
-	        self.warrior.stop();
+	        anim.stop();
 	    }
 	    else if(e.keyCode === 37) {
 	        leftPressed = false;
-	        self.warrior2.stop();
+	        anim.stop();
+	    }
+	    else if(e.keyCode === 32) {
+	        anim.textures = frames3;
+	        anim.gotoAndPlay(0);
+	        anim.loop = false;
 	    }
 	}
 
-	this.draw = function() {
+	let draw = () => {
 		if(rightPressed) {
-    		self.warrior2.y = self.warrior.y;
-    		self.warrior2.x = self.warrior.x += 1;		
+    		anim.x += 1;		
 		}
 		else if(downPressed) {
-		    self.warrior.x = self.warrior2.x;
-		    self.warrior.y  = self.warrior2.y += 1;
+		    anim.y += 1;
 		}
 		else if(upPressed) {
-			self.warrior.x = self.warrior2.x;
-			self.warrior.y  = self.warrior2.y -= 1;
+			anim.y -= 1;
 		}
 		else if(leftPressed) {
-			self.warrior2.y = self.warrior.y;
-    		self.warrior2.x = self.warrior.x -= 1;
+    		anim.x -= 1;
 		}
 
-		requestAnimationFrame(self.draw);
+		requestAnimationFrame(draw);
 	}	
 
-	this.draw();
+	draw();
 
 };
 
